2612bdfcf20d2a4982c60198fb11b9c40bf3c0df
chen jiyan <3036789610@qq.com>
2026-02-27 16:55:34 +0800

    sh better

--- Pytorch-UNet/run_training.sh
+++ Pytorch-UNet/run_training.sh
@@ -6,18 +6,20 @@ source /share/home/zjm/anaconda3/bin/activate zjm
 # Navigate to the project directory
 cd /share/home/zjm/ORO/Pytorch-UNet
 
+mkdir -p runs
+
 # Create a timestamped log file
 LOG_FILE="runs/training_$(date +%Y%m%d_%H%M%S).log"
 
 # Run the training script and log output using tee
 python train.py \
-    --epochs 50 \
+    --epochs 200 \
     --batch-size 4 \
-    --learning-rate 1e-3 \
+    --learning-rate 1e-4 \
     --scale 0.5 \
     --validation 10 \
     --classes 4 \
-    --mask-channels 4 | tee "$LOG_FILE"
+    --mask-channels 4 2>&1 | tee "$LOG_FILE"
 
 # Compress the runs directory
 BACKUP_FILE="runs_backup_$(date +%Y%m%d_%H%M%S).tar.gz"
